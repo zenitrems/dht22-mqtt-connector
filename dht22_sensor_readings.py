00770c0d7485f61acabcebaa2cc5dc2b212e38ad
zenitrems <46614852+zenitrems@users.noreply.github.com>
2024-08-04 17:20:33 -0500

    Format

--- dht22_sensor_readings.py
+++ dht22_sensor_readings.py
@@ -6,19 +6,20 @@ import logging
 
 dht_device = adafruit_dht.DHT22(D17)
 
+
 def fetch_humidity():
-  try:
-    humidity = dht_device.humidity
-    return humidity
-  except Exception as e:
-    logging.error(e) 
-    raise e
+    try:
+        humidity = dht_device.humidity
+        return humidity
+    except Exception as e:
+        logging.error(e)
+        raise e
+
 
 def fetch_temperature():
-  try:
-    temperature = dht_device.temperature
-    return temperature
-  except Exception as e:
-    logging.error(e)
-    raise e
-
+    try:
+        temperature = dht_device.temperature
+        return temperature
+    except Exception as e:
+        logging.error(e)
+        raise e
--- mqtt_connector.py
+++ mqtt_connector.py
@@ -12,21 +12,24 @@ from typing import Any, Dict
 from datetime import datetime
 
 load_dotenv()
-BROKER_ADDRESS = os.environ.get('BROKER_ADDRESS')
-BROKER_PORT = os.environ.get('BROKER_PORT')
-CLIENT_ID = os.environ.get('CLIENT_ID')
-CLIENT_USER = os.environ.get('CLIENT_USER')
-CLIENT_PSSWD = os.environ.get('CLIENT_PSSWD')
-PUBLISH_INTERVAL = os.environ.get('PUBLISH_INTERVAL')
+BROKER_ADDRESS = os.environ.get("BROKER_ADDRESS")
+BROKER_PORT = os.environ.get("BROKER_PORT")
+CLIENT_ID = os.environ.get("CLIENT_ID")
+CLIENT_USER = os.environ.get("CLIENT_USER")
+CLIENT_PSSWD = os.environ.get("CLIENT_PSSWD")
+PUBLISH_INTERVAL = os.environ.get("PUBLISH_INTERVAL")
+
 
 def main():
     logging.basicConfig(
-        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+        format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
         level=logging.DEBUG,
-        datefmt='%Y-%m-%d %H:%M:%S'
+        datefmt="%Y-%m-%d %H:%M:%S",
     )
 
-    mqtt_client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2, client_id=CLIENT_ID, clean_session=False)
+    mqtt_client = mqtt.Client(
+        mqtt.CallbackAPIVersion.VERSION2, client_id=CLIENT_ID, clean_session=False
+    )
     mqtt_client.username_pw_set(CLIENT_USER, CLIENT_PSSWD)
     mqtt_client.max_queued_messages_set(0)
     mqtt_client.connect(BROKER_ADDRESS, int(BROKER_PORT))
@@ -35,37 +38,42 @@ def main():
     periodically_publish_dht22_data(mqtt_client)
     mqtt_client.loop_forever()
 
+
 def on_connect(client, userdata, flags, reason_code, properties):
     if flags.session_present:
-        logging.debug('Client connected with flags: {}'.format(flags))
+        logging.debug("Client connected with flags: {}".format(flags))
     if reason_code == 0:
-        logging.debug('{}  MQTT Client Connected to {}  {}'.format("#" * 30, BROKER_ADDRESS, "#" * 30))
+        logging.debug(
+            "{}  MQTT Client Connected to {}  {}".format(
+                "#" * 30, BROKER_ADDRESS, "#" * 30
+            )
+        )
     if reason_code > 0:
-        logging.debug('Client connected with return code: {}'.format(reason_code))
-        #error processing
+        logging.debug("Client connected with return code: {}".format(reason_code))
+        # error processing
+
 
 def on_disconnect(client: mqtt.Client, userdata: Any, flags: Dict, rc: int) -> None:
     logging.warning("MQTT Client Disconnecting...")
 
 
-
-
 def periodically_publish_dht22_data(client: mqtt.Client) -> None:
-    while(True):
+    while True:
         try:
-            dts = datetime.now() 
+            dts = datetime.now()
             res = {
-                'temperature': dht22.fetch_temperature(),
-                'humidity': dht22.fetch_humidity(),
-                'dts' : dts.strftime("%d/%m/%Y %H:%M:%S")
+                "temperature": dht22.fetch_temperature(),
+                "humidity": dht22.fetch_humidity(),
+                "dts": dts.strftime("%d/%m/%Y %H:%M:%S"),
             }
-            client.publish('sensor/{}'.format(CLIENT_ID), json.dumps(res))
-            logging.info('Published Data: ')
-            logging.info(json.dumps(res)) 
+            client.publish("sensor/{}".format(CLIENT_ID), json.dumps(res))
+            logging.info("Published Data: ")
+            logging.info(json.dumps(res))
             time.sleep(15)
         except Exception as e:
             logging.error(e)
             continue
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
