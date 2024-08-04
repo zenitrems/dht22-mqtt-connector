dfa4ba96365fef7f619f9e18f0236a9cc796205d
zenitrems <46614852+zenitrems@users.noreply.github.com>
2024-08-04 17:18:25 -0500

    paho.mqtt CallbackAPI 2

--- mqtt_connector.py
+++ mqtt_connector.py
@@ -15,19 +15,19 @@ load_dotenv()
 BROKER_ADDRESS = os.environ.get('BROKER_ADDRESS')
 BROKER_PORT = os.environ.get('BROKER_PORT')
 CLIENT_ID = os.environ.get('CLIENT_ID')
+CLIENT_USER = os.environ.get('CLIENT_USER')
 CLIENT_PSSWD = os.environ.get('CLIENT_PSSWD')
-
+PUBLISH_INTERVAL = os.environ.get('PUBLISH_INTERVAL')
 
 def main():
     logging.basicConfig(
-        filename='logs/application.log',
         format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
         level=logging.DEBUG,
         datefmt='%Y-%m-%d %H:%M:%S'
     )
 
-    mqtt_client = mqtt.Client(client_id=CLIENT_ID, clean_session=False)
-    mqtt_client.username_pw_set(CLIENT_ID, CLIENT_PSSWD)
+    mqtt_client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2, client_id=CLIENT_ID, clean_session=False)
+    mqtt_client.username_pw_set(CLIENT_USER, CLIENT_PSSWD)
     mqtt_client.max_queued_messages_set(0)
     mqtt_client.connect(BROKER_ADDRESS, int(BROKER_PORT))
     mqtt_client.on_connect = on_connect
@@ -35,16 +35,18 @@ def main():
     periodically_publish_dht22_data(mqtt_client)
     mqtt_client.loop_forever()
 
+def on_connect(client, userdata, flags, reason_code, properties):
+    if flags.session_present:
+        logging.debug('Client connected with flags: {}'.format(flags))
+    if reason_code == 0:
+        logging.debug('{}  MQTT Client Connected to {}  {}'.format("#" * 30, BROKER_ADDRESS, "#" * 30))
+    if reason_code > 0:
+        logging.debug('Client connected with return code: {}'.format(reason_code))
+        #error processing
 
 def on_disconnect(client: mqtt.Client, userdata: Any, flags: Dict, rc: int) -> None:
     logging.warning("MQTT Client Disconnecting...")
 
-
-def on_connect(client: mqtt.Client, userdata: Any, flags: Dict, rc: int) -> None:
-    logging.debug('{}  MQTT Client Connected to {}  {}'.format(
-        "#" * 30, BROKER_ADDRESS, "#" * 30))
-    logging.debug('Client connected with flags: {}'.format(flags))
-    logging.debug('Client connected with return code: {}'.format(rc))
 
 
 
@@ -53,14 +55,14 @@ def periodically_publish_dht22_data(client: mqtt.Client) -> None:
         try:
             dts = datetime.now() 
             res = {
-                'temp': dht22.fetch_temperature(),
+                'temperature': dht22.fetch_temperature(),
                 'humidity': dht22.fetch_humidity(),
                 'dts' : dts.strftime("%d/%m/%Y %H:%M:%S")
             }
             client.publish('sensor/{}'.format(CLIENT_ID), json.dumps(res))
             logging.info('Published Data: ')
             logging.info(json.dumps(res)) 
-            time.sleep(2)
+            time.sleep(15)
         except Exception as e:
             logging.error(e)
             continue
